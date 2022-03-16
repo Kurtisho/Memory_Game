79ea5ee1aea7f313d8576d631ad1e754351753e4
Kurtisho <Kurtisjeremyho@hotmail.com>
2022-03-16 13:40:53 -0700

    Add scene changes

--- src/main/ui/MemoryGameGUI.java
+++ src/main/ui/MemoryGameGUI.java
@@ -1,11 +1,11 @@
 package ui;
 
+import model.Board;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
 
 
 public class MemoryGameGUI extends JFrame implements ActionListener {
@@ -13,58 +13,74 @@ public class MemoryGameGUI extends JFrame implements ActionListener {
     private static final int PLAYING_WIDTH = 1200;
     private static final int PLAYING_HEIGHT = 900;
 
-    private static final int HUD_WIDTH = 600;
-    private static final int HUD_HEIGHT = 600;
+    private static final int HUD_WIDTH = 800;
+    private static final int HUD_HEIGHT = 800;
 
-    private static final String title = "Memory Game";
+    private static final String labelSmt = "Memory Game";
 
     private static final Color BACKGROUND_COLOUR = new Color(36, 30, 30);
-    private static final Font TITLE_FONT = new Font("Spectre", Font.PLAIN, 50);
+    private static final Font TITLE_FONT = new Font("Spectre", Font.PLAIN, 80);
     private static final Color TITLE_COLOUR = new Color(208, 200, 200);
-    private static final LayoutManager MENU_LAYOUT = new GridLayout(3,1,20,20);
+    private static final LayoutManager MENU_LAYOUT = new GridLayout(3,1,50,50);
 
     private static final Color MAIN_MENU_BUT_COLOUR = new Color(86, 78, 78);
-    private static final Font MENU_BUTTON_FONT = new Font("Spectre", Font.PLAIN, 20);
-    private static final Dimension MAIN_MENU_BUT_DIM = new Dimension(140, 36);
+    private static final Font MENU_BUTTON_FONT = new Font("Spectre", Font.PLAIN, 50);
+    private static final Dimension MAIN_MENU_BUT_DIM = new Dimension(500, 250);
 
+//    private static final LayoutManager playingBoard = new GridLayout(4,4,10,10); // change later
 
-    private JPanel panel;
+    private Board board;
+
+    private CardLayout cl;
+
+    private JPanel mainPanel; // panel everything is worked on
+
+    private JPanel mainMenuPanel; //first action panel
     private JPanel menuLayout;
+
+    private JPanel gamePanel; // second action panel
+    private JPanel gameBoard;
+
+    private JPanel smt;
+
+    private JFrame frame;
 
 
 
     // main menu
     public MemoryGameGUI() {
-        super(title);
+        cl = new CardLayout();
+        frame = new JFrame(labelSmt);
+        frame.setSize(HUD_WIDTH,HUD_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // change later
+        frame.setResizable(false);
+        frame.setVisible(true);
+
         mainMenuHUD();
-        // add json
+
+
+        // add json later
     }
 
     private void mainMenuHUD() {
-        this.setSize(HUD_WIDTH,HUD_HEIGHT);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // change later
-        this.setResizable(false);
-        initMenu();
+        mainPanel = new JPanel();
+        mainPanel.setLayout(cl);
+        frame.add(mainPanel);
+        initMenu(); // makes the menu
 
-        this.setVisible(true);
-
+        cl.show(mainPanel, "menu");
     }
 
-    //MODIFIES: this
-    //EFFECTS: initializes main menu panel, buttons and labels
-    private void initMenu() {
-        panel = new JPanel();
-        panel.setBackground(BACKGROUND_COLOUR);
-        this.setContentPane(panel);
 
-        this.setLayout(null);
+    private void initMenu() {
+        mainMenuPanel = new JPanel();
+        mainMenuPanel.setBackground(BACKGROUND_COLOUR);
+        mainMenuPanel.setLayout(null);
 
         initTitle();
-        initMenuButtons();
+        initMenuOptions();
 
-//        this.setVisible(true);
-
-
+        mainPanel.add(mainMenuPanel, "menu");
     }
 
     private void initTitle() {
@@ -72,30 +88,24 @@ public class MemoryGameGUI extends JFrame implements ActionListener {
         title.setFont(TITLE_FONT);
         title.setForeground(TITLE_COLOUR);
         title.setHorizontalAlignment(JLabel.CENTER);
-        title.setBounds(0,50,550,50);
+        title.setBounds(100,50,550,100);
+        mainMenuPanel.add(title);
 
-        panel.add(title);
     }
 
-
-    private void initMenuButtons() {
-
+    private void initMenuOptions() {
         menuLayout = new JPanel();
         menuLayout.setLayout(MENU_LAYOUT);
         menuLayout.setBackground(BACKGROUND_COLOUR);
-        menuLayout.setBounds(160,200,250,200);
+        menuLayout.setBounds(160,200,500,400);
 
         newBoardButton();
         loadBoardButton();
         quitGameButton();
 
-        panel.add(menuLayout);
-
-
+        mainMenuPanel.add(menuLayout);
 
     }
-
-
 
     private void newBoardButton() {
         JButton gameBut = new JButton("Create New Board");
@@ -103,12 +113,12 @@ public class MemoryGameGUI extends JFrame implements ActionListener {
         gameBut.setForeground(TITLE_COLOUR);
         gameBut.setFont(MENU_BUTTON_FONT);
 
-        // add listener for response
+        gameBut.setActionCommand("Create Board");
+        gameBut.addActionListener(this);
 
         gameBut.setPreferredSize(MAIN_MENU_BUT_DIM);
         gameBut.setToolTipText("Start the game!");
         menuLayout.add(gameBut);
-
 
     }
 
@@ -118,7 +128,8 @@ public class MemoryGameGUI extends JFrame implements ActionListener {
         loadBut.setForeground(TITLE_COLOUR);
         loadBut.setFont(MENU_BUTTON_FONT);
 
-        // add listener for response
+        loadBut.setActionCommand("Load Board");
+        loadBut.addActionListener(this);
 
         loadBut.setPreferredSize(MAIN_MENU_BUT_DIM);
         loadBut.setToolTipText("Load previously saved board");
@@ -131,19 +142,43 @@ public class MemoryGameGUI extends JFrame implements ActionListener {
         quitBut.setForeground(TITLE_COLOUR);
         quitBut.setFont(MENU_BUTTON_FONT);
 
-        // add listener for response
+        quitBut.setActionCommand("Quit Game");
+        quitBut.addActionListener(this);
 
         quitBut.setPreferredSize(MAIN_MENU_BUT_DIM);
         quitBut.setToolTipText("Close the game");
         menuLayout.add(quitBut);
     }
 
+    private void setGameUp() {
+        gamePanel = new JPanel();
+        gamePanel.setBackground(BACKGROUND_COLOUR);
+        gamePanel.setLayout(null);
+
+        
+
+        mainPanel.add(gamePanel, "game");
+    }
 
 
 
-    // listens to buttons to do action
+
+
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        switch (e.getActionCommand()) {
+            case "Create Board" :
+                setGameUp();
+                cl.show(mainPanel, "game");
+                break;
+            case "Load Board":
+                System.out.println("Loading");
+                break;
+            case "Quit Game":
+                System.out.println("Quitting Game");
+        }
     }
 }
