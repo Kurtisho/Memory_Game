a7d8d99945a0cdae6b2979393a5a185ece7f3606
Kurtis <kurtisho@student.ubc.ca>
2022-04-01 15:35:27 -0700

    Final update

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -10,7 +10,6 @@ public class Main {
 //        } catch (FileNotFoundException e) {
 //            System.out.println("Unable to run application: file not found");
 //        }
-
         new MemoryGameGUI();
     }
 }
